b77b210810b3c48a7d0c3a4219d90d6418010176
gusticahya <gusticahyax@yahoo.com>
2026-03-12 17:17:14 +0800

    chore: Add CORS headers to Next.js API routes configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // Config Next.js
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", //TODO: Adjust to domain flutter
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,DELETE,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 }
 
 export default nextConfig
